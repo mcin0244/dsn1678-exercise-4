b2a48ad6506d959c2cac72218a9de5c3a6dfa980
mcin0244 <mcin0244@algonquinlive.com>
2014-10-07 10:11:53 -0400

    Exercise 4

--- js/main.js
+++ js/main.js
@@ -1,9 +1,24 @@
 var $win = $(window);
 var $sun = $('.sun');
+var $sunSection = $('.sun-section');
+var $dipperSection = $('.dipper-section');
+var $dipper = $('.dipper');
+var $shuttle = $('.shuttle')
+var $shuttleSection = $('.shuttle-section')
+
 
 $win.on('scroll', function () {
 	var scrollPos = $win.scrollTop();
 
-	$sun.css('transform', 'rotate(' + scrollPos + 'deg)');
-
+	$sun.css('transform', 'rotate(' + scrollPos / 2 + 'deg)');
+	$sunSection.css('background-position', 'center ' + scrollPos / 3 + 'px');
 });
+
+
+$dipperSection.waypoint(function () {
+	$dipper.addClass('js-dipper-fade');
+}, { offset: '50%' });
+
+$shuttleSection.waypoint(function () {
+	$shuttle.addClass('js-shuttle-fade');
+}, { offset: '50%' });
